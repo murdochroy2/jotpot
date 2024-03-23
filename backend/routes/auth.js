02768db158ca661e3f0017804afb462c031260fe
Rohi Modak <rohi@localhost>
2024-03-23 12:49:39 +0530

    Multiple changes: 1. change collection name 2. change express app port 3. add dependencies, libraries 4. skip index creation 5. change endpoint name, authenticate using jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,57 @@
 const express = require('express')
+const { body, validationResult } = require('express-validator');
+
 const router = express.Router()
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = "the jwt secret"
 
-router.post('/', 
-    (req, res) => {
-        console.log(req.body)
-        const user = new User(req.body)
-        user.save()
-        res.send(req.body)
+router.post('/createuser',
+    [
+        body('name').notEmpty(),
+        body('password').isLength({ min: 5 }),
+    ],
+    async (req, res) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            // return res.status(400).json({Errors: result.array()});
+            return res.send({ errors: result.array() });
+        }
+        try {
+            let user0 = await User.findOne({ email: req.body.email })
+            if (user0) {
+                return res.status(400).send({ error: "User already exists" })
+            }
+            console.log("Creating User")
+            const salt = await bcrypt.genSalt(10)
+            const securePassword = await bcrypt.hash(req.body.password, salt)
+            const user = new User({ name: req.body.name, email: req.body.email, password: securePassword })
+            user.save()
+                .then(
+                    () => {
+                        console.log("User Created")
+                        const userData = {
+                            user: {
+                                id: user.id
+                            }
+                        }
+                        const authToken = jwt.sign(userData, JWT_SECRET)
+                        res.send({authToken})
+                    }
+                )
+                .catch(
+                    (err) => {
+                        console.log({ error: err.message })
+                        res.send({ error: err.message })
+                    }
+                )
+        }
+        catch (err) {
+            console.log({ error: err.message })
+            res.status(500).send("Some error occurred")
+        }
+
     }
 )
 
