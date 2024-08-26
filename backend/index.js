6e89c2fb37c3d5b3a3916fddca405596b47da3ce
Rohi Modak <rohi@localhost>
2024-08-26 23:36:08 +0530

    Get details from environment

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const connectToMongo = require("./db");
 const express = require('express')
 var cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 8080
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', require('./routes/auth'));
