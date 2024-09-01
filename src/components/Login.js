4e010a90e54dbd7eba62299240b669889539f44d
Rohi Modak <rohi@localhost>
2024-09-01 13:57:59 +0530

    Prefix with REACT_APP_ (the environment variables)

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 const Login = (props) => {
     const emptyCredentials = { email: "", password: "" }
     const [credentials, setCredentials] = useState(emptyCredentials)
-    const host = `${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`
+    const host = `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_HOST_PORT}`
     const navigate = useNavigate()
     const { showAlert } = props
     const handleLoginFormSubmit = async (e) => {
